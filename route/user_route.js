594e147e5a87f352e361d4acfb66c0a5a536bec3
rakib <jhadul.islam@itconquest.com>
2020-04-03 10:39:46 +0600

    Add

--- route/user_route.js
+++ route/user_route.js
@@ -1,25 +1,11 @@
 const express = require('express');
 let router = express.Router();
 
-const UserModel = require('../model/user');
-const mailService = require('../utils/mail_service');
-router.post('/', async (request, response) => {
-    try {
-        const user = new UserModel({
-            userName: request.body.userName,
-            userEmail: request.body.userEmail,
-            password: request.body.password,
-            phoneNumber: request.body.phoneNumber,
-            userType: request.body.userType
-        });
-        let newVar = await user.save();
-        if (newVar) {
-            mailService.sendMail(user['userEmail']);
-        }
-        response.status(200).send(newVar);
-    } catch (e) {
-        response.status(500).send(e);
-    }
+const UserService = require('../service/user-service');
+const user = new UserService();
+
+router.post('/',async (request, response) => {
+    await user.saveUser(request, response);
 });
 
 module.exports = router;
---
+++ service/user-service.js
@@ -0,0 +1,25 @@
+const UserModel = require('../model/user');
+const mailService = require('../utils/mail_service');
+module.exports = class UserService {
+    saveUser(request, response) {
+        try {
+            const user = new UserModel({
+                userName: request.body.userName,
+                userEmail: request.body.userEmail,
+                password: request.body.password,
+                phoneNumber: request.body.phoneNumber,
+                userType: request.body.userType
+            });
+            let newVar = user.save();
+            if (newVar) {
+                mailService.sendMail(user['userEmail']);
+            }
+            response.status(200).send(newVar);
+        } catch (e) {
+            response.status(500).send(e);
+        }
+    }
+
+
+
+}
